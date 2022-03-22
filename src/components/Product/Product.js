e74550e183c3e06279ba7c5b6ae61c1e4fe8934f
Misrat121 <you@example.com>
2022-03-22 12:58:39 +0600

    set eventhandler

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -2,7 +2,11 @@ import React from 'react';
 import './Product.css'
 
 const Product = (props) => {
+    
+
     const{name, img, seller, price, ratings} = props.product;
+   
+    
     return (
         <div className='product'>
             <img src ={img} alt =""></img>
@@ -12,7 +16,7 @@ const Product = (props) => {
             <p><small>Seller: {seller} </small></p>
             <p><small>Ratings: {ratings} stars</small></p>
              </div>
-             <button className='btn-cart'>
+             <button onClick={() => props.handleAddToCart(props.product)} className='btn-cart'>
                  <p>Add to Cart</p>
              </button>
         </div>
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -9,12 +9,18 @@ const Shop = () => {
         .then(res=>res.json())
         .then(data => setProducts(data))
     }, [])
+
+    const handleAddToCart = (product) =>{
+        console.log(product)
+    }
     return (
         <div className='shop-container'>
           <div className="product-container">
             {
-                products.map(product=><Product key = {product.id}
-                product = {product}
+                products.map(product=><Product 
+                    key = {product.id}
+                    product = {product}
+                    handleAddToCart={handleAddToCart}
                 ></Product>)
             }
           </div>
