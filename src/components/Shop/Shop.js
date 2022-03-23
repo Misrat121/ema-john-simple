d9e4459c4912b97b5b472ead38069c9cbfbbe687
Misrat121 <you@example.com>
2022-03-23 11:56:47 +0600

    cart data set on the local storage

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import {addToDb} from '../../utilities/fakedb';
 import Product from '../Product/Product';
 import Cart from '../Cart/Cart';
 import './Shop.css';
@@ -17,6 +18,8 @@ const Shop = () => {
         // cart.push(product);
         const newCart = [...cart, product]
         setCart(newCart);
+        addToDb(product.id);
+
     }
     return (
         <div className='shop-container'>
